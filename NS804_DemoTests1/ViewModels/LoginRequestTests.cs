00f4ba78144d73fa9899ad03f023d379b7b4990e
Jafet Benítez <jafetg.benitez@gmail.com>
2021-08-29 13:14:22 -0400

    [Tests]: unitests for Login and Register 's payloads

---
+++ NS804_DemoTests1/ViewModels/LoginRequestTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NS804_Demo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NS804_Demo.ViewModels.Tests
+{
+    [TestClass()]
+    public class LoginRequestTests
+    {
+        [TestMethod()]
+        public void IsValidTest_EmptyConstructor()
+        {
+            LoginRequest request = new LoginRequest();
+
+            Assert.Equals(request.IsValid(), false);
+        }
+
+
+
+        [TestMethod()]
+        public void IsValidTest_JustUser()
+        {
+            LoginRequest request = new LoginRequest();
+            request.UserName = "SuperUser";
+
+            Assert.Equals(request.IsValid(), false);
+        }
+
+
+        [TestMethod()]
+        public void IsValidTest_JustPassword()
+        {
+            LoginRequest request = new LoginRequest();
+            request.Password = "SuperPassword";
+
+            Assert.Equals(request.IsValid(), false);
+        }
+
+        [TestMethod()]
+        public void IsValidTest_HappyCase()
+        {
+            LoginRequest request = new LoginRequest();
+            request.Password = "SuperPassword";
+            request.UserName = "SuperUser";
+            Assert.Equals(request.IsValid(), true);
+        }
+    }
+}
---
+++ NS804_DemoTests1/ViewModels/RegisterRequestTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NS804_Demo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NS804_Demo.ViewModels.Tests
+{
+    [TestClass()]
+    public class RegisterRequestTests
+    {
+        [TestMethod()]
+        public void IsValidTest_EmptyConstructor()
+        {
+            RegisterRequest request = new RegisterRequest();
+
+            Assert.Equals(request.IsValid(), false);
+        }
+
+
+
+        [TestMethod()]
+        public void IsValidTest_JustUser()
+        {
+            RegisterRequest request = new RegisterRequest();
+            request.UserName = "SuperUser";
+
+            Assert.Equals(request.IsValid(), false);
+        }
+
+
+        [TestMethod()]
+        public void IsValidTest_JustPassword()
+        {
+            RegisterRequest request = new RegisterRequest();
+            request.Password = "SuperPassword";
+
+            Assert.Equals(request.IsValid(), false);
+        }
+
+        [TestMethod()]
+        public void IsValidTest_PasswordMismatch()
+        {
+            RegisterRequest request = new RegisterRequest();
+            request.UserName = "SuperUser";
+            request.Password = "SuperPassword";
+            request.ConfirmPassword = "SuperPassword1";
+
+            Assert.Equals(request.IsValid(), false);
+        }
+
+
+        [TestMethod()]
+        public void IsValidTest_HappyCase()
+        {
+            RegisterRequest request = new RegisterRequest();
+            request.UserName = "SuperUser";
+            request.Password = "SuperPassword";
+            request.ConfirmPassword = "SuperPassword";
+
+            Assert.Equals(request.IsValid(), true);
+        }
+    }
+
+}
